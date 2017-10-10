c92a52e26cbd80881e92efbb75edf09db082ceae
Besher <besher@camtrssc9206.local>
2017-10-10 17:50:50 -0400

    Initial Commit

---
+++ Lion Search/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Lion Search
+//
+//  Created by Besher on 2017-10-10.
+//  Copyright © 2017 Besher Al Maleh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
